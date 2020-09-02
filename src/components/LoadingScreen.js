3a85a51f18e077ac42c52ee39ea354ce343565c7
Aaron Zheng <aaronhaowenzheng@gmail.com>
2020-09-02 20:24:43 +0800

    I have done some stuff

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,13 @@
+//Loading screen
+import React from 'react'
+import '../App.css'
+
+function LoadingScreen(){
+  return (
+    <div className="loading_screen_container">
+      <h1>Loading...</h1>
+      <h2>Please wait for a moment</h2>
+    </div>
+  )
+}
+export default LoadingScreen;
